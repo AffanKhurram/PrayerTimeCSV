cf21ace4a50abde2ed537a9fcec8234a797266e0
Affan Khurram <affankhurram1@gmail.com>
2021-09-16 21:05:34 -0500

    added date range

--- main.js
+++ main.js
@@ -6,11 +6,29 @@ async function fetchAsync (url) {
     return data;
 }
 
-function createArray (data, options) {
-    var arr = data.data;
+function createArray (arr, options, start, end) {
     var csvArray = [['Subject', 'Start date', 'Start time']];
 
-    arr.forEach(function(day) {
+    for (var i = 0; i < arr.length; i++) {
+        var day = arr[i];
+        // Skip dates
+        if (day.date.gregorian.year == start[0] && day.date.gregorian.month.number == parseInt(start[1]) && parseInt(day.date.gregorian.day) < parseInt(start[2])) {
+            continue;
+        }
+        // stop
+        if (day.date.gregorian.year == end[0] && day.date.gregorian.month.number == parseInt(end[1]) && parseInt(day.date.gregorian.day) == parseInt(end[2])) {
+            if (options.fajr)
+                csvArray.push(['Fajr', day.date.gregorian.month.number + '/' + day.date.gregorian.day + '/' + day.date.gregorian.year, day.timings.Fajr.slice(0, -6)]);
+            if (options.dhuhr)
+                csvArray.push(['Dhuhr', day.date.gregorian.month.number + '/' + day.date.gregorian.day + '/' + day.date.gregorian.year, day.timings.Dhuhr.slice(0, -6)]);
+            if (options.asr)
+                csvArray.push(['Asr', day.date.gregorian.month.number + '/' + day.date.gregorian.day + '/' + day.date.gregorian.year, day.timings.Asr.slice(0, -6)]);
+            if (options.maghrib)
+                csvArray.push(['Maghrib', day.date.gregorian.month.number + '/' + day.date.gregorian.day + '/' + day.date.gregorian.year, day.timings.Maghrib.slice(0, -6)]);
+            if (options.isha)
+                csvArray.push(['Isha', day.date.gregorian.month.number + '/' + day.date.gregorian.day + '/' + day.date.gregorian.year, day.timings.Isha.slice(0, -6)]);
+            break;
+        }
         if (options.fajr)
             csvArray.push(['Fajr', day.date.gregorian.month.number + '/' + day.date.gregorian.day + '/' + day.date.gregorian.year, day.timings.Fajr.slice(0, -6)]);
         if (options.dhuhr)
@@ -21,7 +39,7 @@ function createArray (data, options) {
             csvArray.push(['Maghrib', day.date.gregorian.month.number + '/' + day.date.gregorian.day + '/' + day.date.gregorian.year, day.timings.Maghrib.slice(0, -6)]);
         if (options.isha)
             csvArray.push(['Isha', day.date.gregorian.month.number + '/' + day.date.gregorian.day + '/' + day.date.gregorian.year, day.timings.Isha.slice(0, -6)]);
-    });
+    }
 
     return csvArray;
 }
@@ -42,13 +60,32 @@ function downloadFile (data) {
     link.click();
 }
 
-// document.getElementById("input").addEventListener("submit", function(e) {
-//     console.log(document.getElementById('input')['address']);
-// });
-
-document.getElementById('submit').addEventListener('click', function(e) {
+document.getElementById('submit').addEventListener('click', async function(e) {
     var form = document.getElementById('input');
-    var url = "http://api.aladhan.com/v1/calendarByAddress?address=" + form.address.value + "&method=2&year=" + form.year.value;
+    var start = form.start.value.split('-');
+    var end = form.end.value.split('-');
+    var s = parseInt(start[0] + start[1]);
+    var e = parseInt(end[0] + end[1])
+    var arr = [];
+
+    while (s <= e) {
+        var month = s%100;
+        var year = Math.floor(s/100);
+        var url = "https://api.aladhan.com/v1/calendarByAddress?address=" + form.address.value + "&method=2&month=" + month + "&year=" + year;
+        s++;
+        if (s % 100 > 12) {
+            s -= 12;
+            s += 100;
+        }
+        await fetchAsync(url)
+            .then(function(data) {
+                arr = arr.concat(data.data);
+            })
+            .catch(function(err) {
+                console.log(err);
+            });
+    }
+    
     var options = {
         fajr: document.getElementById("Fajr").checked,
         dhuhr: document.getElementById("Dhuhr").checked,
@@ -56,50 +93,7 @@ document.getElementById('submit').addEventListener('click', function(e) {
         maghrib: document.getElementById("Maghrib").checked,
         isha: document.getElementById("Isha").checked
     }
+
+    downloadFile(createArray(arr, options, start, end));
         
-    fetchAsync("https://api.aladhan.com/v1/calendarByAddress?address=Richardson,TX,USA&method=2&month=9&year=2021")
-        .then(function(data) {
-            var arr = createArray(data, options);
-            // arr.forEach(function(row) {
-            //     console.log(row);
-            // });
-            downloadFile(arr);
-        })
-        .catch(function(err) {
-            console.log(err);
-        })
 });
-
-// fetchAsync("http://api.aladhan.com/v1/calendarByAddress?address=Richardson,TX,USA&method=2&month=9&year=2021")
-//     .then(function(data) {
-//         var arr = createArray(data);
-//         arr.forEach(function(row) {
-//             console.log(row);
-//         });
-//         //downloadFile(arr);
-//     })
-//     .catch(function(err) {
-//         console.log(err);
-//     })
-
-// var arr = [
-//     ['subject', 'start date', 'start time'],
-//     ['fajr', '9/1/2021', '13:39'],
-//     ['dhuhr, 9/1/2021', '13:3']
-// ];
-
-// let csvContent = "data:text/csv;charset=utf-8,";
-
-// arr.forEach(function(row) {
-//     let line = row.join(',');
-//     csvContent += line + '\r\n';
-// });
-
-// var encodedUri = encodeURI(csvContent);
-// var link = document.createElement("a");
-// link.setAttribute("href", encodedUri);
-// link.setAttribute("download", "prayer_timings.csv");
-// console.log(document.body);
-// document.body.appendChild(link); // Required for FF
-
-// link.click(); // This will download the data file named "my_data.csv".
